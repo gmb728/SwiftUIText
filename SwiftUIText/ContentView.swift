c0f80edc8772392c6494bf7725e4ef9c2e2a383f
Sophia <gmb728@gmail.com>
2020-02-27 13:25:13 +0800

    Initial Commit

---
+++ SwiftUIText/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIText
+//
+//  Created by Chang Sophia on 2/27/20.
+//  Copyright © 2020 Chang Sophia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
